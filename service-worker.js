64af56627d0c1399cdd2f1c5d58ab0ceeaf9f79d
schulhelfer <262822418+schulhelfer@users.noreply.github.com>
2026-03-06 17:14:21 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,86 @@
+const VERSION = 1;
+const CACHE_NAME = `buecher-v${VERSION}`;
+const OFFLINE_PAGE = "./index.html";
+
+const APP_SHELL = [
+  OFFLINE_PAGE,
+  "./manifest.webmanifest",
+  "./icon-32x32.png",
+  "./icon-192x192.png",
+  "./icon-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const { request } = event;
+
+  if (request.method !== "GET") {
+    return;
+  }
+
+  if (request.mode === "navigate") {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, responseClone));
+          return response;
+        })
+        .catch(() => caches.match(request).then((cached) => cached || caches.match(OFFLINE_PAGE)))
+    );
+    return;
+  }
+
+  if (new URL(request.url).origin !== self.location.origin) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      if (cached) {
+        return cached;
+      }
+
+      return fetch(request).then((response) => {
+        if (!response || response.status !== 200 || response.type === "opaque") {
+          return response;
+        }
+
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(request, responseClone));
+        return response;
+      });
+    })
+  );
+});
+
+self.addEventListener("message", (event) => {
+  const data = event.data || {};
+
+  if (data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+    return;
+  }
+
+  if (data.type === "GET_VERSION" && event.ports && event.ports[0]) {
+    event.ports[0].postMessage({ version: VERSION });
+  }
+});
